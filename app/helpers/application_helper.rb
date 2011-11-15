116140757b2897036b827f1d77905c61db1b9d18
jpearson <jpearson556@gmail.com>
2011-11-15 11:00:38 -0700

    Added links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,21 @@ module ApplicationHelper
       "#{base_title} - #{@title}"
     end
   end
+  
+  # Image links for home page including header
+  def header_button
+    header_button = image_tag('header1.png', :alt => "header")
+  end
+  
+  def snow_button
+    snow_button = image_tag("snow_removal.gif", :alt => "Snow Removal")
+  end
+  
+  def lawn_button
+    lawn_button = image_tag("lawn_care.gif", :alt => "Lawn Care")
+  end
+  
+  def landscape_button
+    landscape_button = image_tag("landscape_design.gif", :alt => "Landscape Design")
+  end
 end
