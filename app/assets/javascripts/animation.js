957474802c100a7baf3d864e302c3daf5acb9165
jpearson <jpearson556@gmail.com>
2011-12-23 16:13:31 -0700

    changed footer

--- app/assets/javascripts/animation.js
+++ app/assets/javascripts/animation.js
@@ -1,6 +1,6 @@
 // animations scripts for hiding and unhiding, fading, etc.
 $(document).ready(function() {
-	$("#one").delay(1000).fadeIn(1000);
+	$("#one").delay(1500).fadeIn(1000);
 	$("#two").delay(1500).fadeIn(3000);
 	$("#three").delay(2000).fadeIn(4000);
 });
